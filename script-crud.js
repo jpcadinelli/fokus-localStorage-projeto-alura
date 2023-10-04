a206667b774456990843dcec3f6eb031fa5cc369
Joao Pedro Cadinelli <jpcadinelli@gmail.com>
2023-10-04 14:08:39 -0300

    Aula 2 Finalizada

--- script-crud.js
+++ script-crud.js
@@ -1,15 +1,15 @@
 const taskListContainer = document.querySelector('.app__section-task-list');
 
-let tarefas = [
-    {
-        descriao: "Tarefa Concluída",
-        concluida: true
-    },
-    {
-        descriao: "Tarefa Pendente",
-        concluida: false
-    }
-];
+const formTask = document.querySelector('.app__form-add-task');
+const toggleFormTaskBtn = document.querySelector('.app__button--add-task');
+const formLabel = document.querySelector('.app__form-label');
+
+const textArea = document.querySelector('.app__form-textarea');
+
+const cancelFormTaskBtn = document.querySelector('.app__form-footer__button--cancel');
+const btnCancelar = document.querySelector('.app__form-footer__button--cancel');
+
+let tarefas = [];
 
 const taskIconSvg = `
 <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24"
@@ -21,6 +21,11 @@ fill="none" xmlns="http://www.w3.org/2000/svg">
 </svg>
 `;
 
+const limparForm = () => {
+    textArea.value = '';
+    formTask.classList.add('hidden');
+};
+
 function createTask(tarefa) {
     const li = document.createElement('li');
     li.classList.add('app__section-task-list-item');
@@ -31,7 +36,7 @@ function createTask(tarefa) {
     const paragraph = document.createElement('p');
     paragraph.classList.add('app__section-task-list-item-description');
 
-    paragraph.textContent = tarefa.descriao;
+    paragraph.textContent = tarefa.descricao;
 
     li.appendChild(svgIcon);
     li.appendChild(paragraph);
@@ -43,3 +48,28 @@ tarefas.forEach(task => {
     const taskItem = createTask(task);
     taskListContainer.appendChild(taskItem);
 });
+
+toggleFormTaskBtn.addEventListener('click', () => {
+    formLabel.textContent = 'Adicionando tarefa';
+    formTask.classList.toggle('hidden');
+});
+
+formTask.addEventListener('submit', (evento) => {
+    evento.preventDefault();
+    const task = {
+        descricao: textArea.value,
+        concluida: false
+    };
+    tarefas.push(task);
+    const taskItem = createTask(task);
+    taskListContainer.appendChild(taskItem);
+    limparForm();
+});
+
+cancelFormTaskBtn.addEventListener('click', () => {
+    formTask.classList.add('hidden');
+    limparForm();
+});
+
+localStorage.setItem('quantidade', 10);
+console.log(localStorage.getItem('quantidade'))
