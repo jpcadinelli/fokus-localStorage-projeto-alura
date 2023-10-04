e3b6f33ce2025e05b814c67ead88eeb7d9bddfe2
Joao Pedro Cadinelli <jpcadinelli@gmail.com>
2023-10-04 16:42:32 -0300

    Aula 4 Concluida

--- script-crud.js
+++ script-crud.js
@@ -28,6 +28,9 @@ const taskIconSvg = `
 let tarefaSelecionada = null
 let itemTarefaSelecionada = null
 
+let tarefaEmEdicao = null
+let paragrafoEmEdicao = null
+
 const selecionaTarefa = (tarefa, elemento) => {
     
     document.querySelectorAll('.app__section-task-list-item-active').forEach(function (button) {
@@ -48,8 +51,23 @@ const selecionaTarefa = (tarefa, elemento) => {
 }
 
 const limparForm = () => {
+    tarefaEmEdicao = null
+    paragrafoEmEdicao = null
     textarea.value = ''
     formTask.classList.add('hidden')
+}
+
+const selecionaTarefaParaEditar = (tarefa, elemento) => {
+    if(tarefaEmEdicao == tarefa) {
+        limparForm()
+        return
+    }
+
+    formLabel.textContent = 'Editando tarefa'
+    tarefaEmEdicao = tarefa
+    paragrafoEmEdicao = elemento
+    textarea.value = tarefa.descricao
+    formTask.classList.remove('hidden')
 }
 
 function createTask(tarefa) {
@@ -65,6 +83,17 @@ function createTask(tarefa) {
     paragraph.textContent = tarefa.descricao
 
     const button = document.createElement('button')
+
+    button.classList.add('app_button-edit')
+    const editIcon = document.createElement('img')
+    editIcon.setAttribute('src', '/imagens/edit.png')
+
+    button.appendChild(editIcon)
+
+    button.addEventListener('click', (event) => {
+        event.stopPropagation()
+        selecionaTarefaParaEditar(tarefa, paragraph)
+    })
 
     li.onclick = () => {
         selecionaTarefa(tarefa, li)
@@ -83,6 +112,7 @@ function createTask(tarefa) {
 
     li.appendChild(svgIcon)
     li.appendChild(paragraph)
+    li.appendChild(button)
     
     return li
 }
@@ -109,14 +139,18 @@ const updateLocalStorage = () => {
 
 formTask.addEventListener('submit', (evento) => {
     evento.preventDefault()
-    const task = {
-        descricao: textarea.value,
-        concluida: false
+    if(tarefaEmEdicao) {
+        tarefaEmEdicao.descricao = textarea.value
+        paragrafoEmEdicao.textContent = textarea.value
+    } else {
+        const task = {
+            descricao: textarea.value,
+            concluida: false
+        }
+        tarefas.push(task)
+        const taskItem = createTask(task)
+        taskListContainer.appendChild(taskItem)
     }
-    tarefas.push(task)
-    const taskItem = createTask(task)
-    taskListContainer.appendChild(taskItem)
-
     updateLocalStorage()
     limparForm()
 })
